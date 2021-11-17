18d88472d17dc1154e04b5c822eb3831494b09ae
Alim Mughanil Karim <alim.mughanil23@gmail.com>
2021-11-17 11:01:43 +0700

    fix bug data pemeriksaan user dan  menambahkan halaman feedback di admin

--- resources/views/admin/feedback-navbar.blade.php
+++ resources/views/admin/feedback-navbar.blade.php
@@ -6,6 +6,7 @@
       <a href="{{ url('/admin/data-feedback') }}" class="dropdown-item">
         <!-- Message Start -->
         <div class="media">
+          <img src="/img/undraw_profile.svg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
           <div class="media-body">
             @if (empty($data['feedback']))
               <p class="text-sm">No data available to show</p>
